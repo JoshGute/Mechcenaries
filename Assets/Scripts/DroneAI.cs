0ee308ff514d32d713a37599ff25621155e9906f
Linus Chan <linus.chan09@gmail.com>
2017-05-16 01:35:35 -0700

    Spawning script

---
+++ Assets/Scripts/DroneAI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum DroneStates { Move, Follow, Stay, Shoot, Die};
+
+public class DroneAI : MonoBehaviour {
+
+  [SerializeField]
+  private Vector3 MoveDirection;
+
+  [SerializeField]
+  private float moveSpeed;
+
+  DroneStates curState = DroneStates.Move;
+
+	// Use this for initialization
+	void Start ()
+  {
+	}
+
+	// Update is called once per frame
+	void Update ()
+  {
+    if (curState == DroneStates.Move)
+    {
+      gameObject.transform.position = MoveDirection += new Vector3(1 * moveSpeed, 0, 0);
+
+    }
+	}
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+  [SerializeField]
+  private GameObject SpawnObject;
+
+  //Spawn the prefab every x seconds
+  [SerializeField]
+  private float iSpawnEveryXSec;
+
+  private float icurSec;
+
+  //Start spawning once scene is opened?
+  [SerializeField]
+  private bool StartSpawnOnCreate;
+
+  //Whether we are spawning or not.
+  private bool isSpawning;
+
+	// Use this for initialization
+	void Start ()
+  {
+    icurSec = 0;
+
+    if(StartSpawnOnCreate == true)
+    {
+      StartSpawn();
+    }
+	}
+	
+  //Start the spawn loop
+  void StartSpawn()
+  {
+    isSpawning = true;
+  }
+
+  //Stop the spawn loop, reset the curSec to 0.
+  void StopSpawn()
+  {
+    icurSec = 0;
+    isSpawning = false;
+  }
+
+  //Spawns the prefab at position
+  public void SpawnPrefab()
+  {
+    Instantiate(SpawnObject, gameObject.transform);
+  }
+
+	// Update is called once per frame
+	void Update ()
+  {
+		if(isSpawning == true)
+    {
+      icurSec += Time.deltaTime;
+
+      if(icurSec >= iSpawnEveryXSec)
+      {
+        SpawnPrefab();
+        icurSec = 0;
+      }
+
+    }
+	}
+}
