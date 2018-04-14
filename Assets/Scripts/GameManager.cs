8aee022458557d7eef191a1a3f4bddd74e9819a0
Josh Gutenberg <joshguteenberg@gmail.com>
2018-04-14 13:48:03 -0700

    objective systems

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject[] mechs;
+    //private PlayerHolder playerholder;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        //SpawnPlayers();
+    }
+
+    //private void SpawnPlayers()
+    //{
+
+    //}
+
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
