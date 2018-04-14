f05bbe8e5872bb1a6bae70da4fa14180d2f2d7c8
Josh Gutenberg <joshguteenberg@gmail.com>
2018-04-14 13:51:46 -0700

    more

--- Assets/Objective.cs
+++ Assets/Objective.cs
@@ -2,15 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Objective : MonoBehaviour {
+public class Objective : MonoBehaviour
+{
+    [SerializeField]
+    private int Score = 100;
+    private int LifeTime = 20;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        //display objective type and description on screen
+        InvokeRepeating("LifeUpdate", 3, 1);
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        //subtract a second off of lifetime every second
 	}
+
+    private void LifeUpdate()
+    {
+        --LifeTime;
+        if (LifeTime <= 0)
+        {
+            ObjectiveFinshed();
+        }
+    }
+
+    public void Complete()
+    {
+        //send complete event with player that completed it to objective manager
+    }
 }
