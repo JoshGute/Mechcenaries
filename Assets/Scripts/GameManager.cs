06b60f0d443b2721ab264ba8a91480eea1d005a2
Josh Gutenberg <joshguteenberg@gmail.com>
2018-04-21 16:05:18 -0700

    pseudo code ahoy

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,23 +5,48 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public GameObject[] mechs;
+    public GameObject[] spawnPoints;
     //private PlayerHolder playerholder;
 
 
     // Use this for initialization
     void Start()
     {
-        //SpawnPlayers();
+        //FindSpawnPoints();
+        //SpawnPlayers(SpawnPointArray);
     }
 
-    //private void SpawnPlayers()
-    //{
+    private GameObject[] FindSpawnPoints()
+    {
+        //find all the spawnpoints and stuff them in an array for the spawner
+        return null;
+    }
 
-    //}
+    private void SpawnPlayers(GameObject[] spawnPoints_)
+    {
+        //loop through the spawnpoints and spawn the players at different points
+    }
 
 	// Update is called once per frame
 	void Update ()
     {
-		
+		//I'm sure something will go here
 	}
+
+    private void SpawnObjective()
+    {
+        //spawns prefab objective at one of the spawnpoints
+    }
+
+    private void UpdateScore()
+    {
+        //takes event from the score updating
+        //checks scores to see if someone won and calls winner
+        //winner(player that won);
+    }
+
+    private void Winner()
+    {
+        //takes the player that won and stores them in an object to take to the win screen
+    }
 }
